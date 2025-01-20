ee31196aae823726a8322f42aa790ece26160ae9
Luis Ramirez <lramirezsuarez@gmail.com>
2025-01-20 18:51:35 -0500

    Initial Commit

---
+++ ChartsSampleApp/ChartsSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsSampleAppApp.swift
+//  ChartsSampleApp
+//
+//  Created by Luis Alejandro Ramirez Suarez on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ChartsSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
